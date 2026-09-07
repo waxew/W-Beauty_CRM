0b8200824e368df6d5287e756e0053a3f9fecd62
waxew <4622.ar@gmail.com>
2026-09-07 12:44:17 +0330

    تکمیل ماژول اولیه مشتریان CRM با Entity DAO Domain و ViewModel

---
+++ app/src/main/java/com/wbeauty/crm/feature/customer/presentation/CustomerViewModel.kt
@@ -0,0 +1,8 @@
+package com.wbeauty.crm.feature.customer.presentation
+
+// ViewModel مشتریان در نسخه کامل به StateFlow و Repository متصل می‌شود.
+class CustomerViewModel {
+    fun loadCustomers() {
+        // دریافت لیست مشتریان
+    }
+}
