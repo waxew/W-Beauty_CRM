c04166f4f1ae2beae1921dbc7d2043931f15505a
waxew <4622.ar@gmail.com>
2026-09-07 12:18:38 +0330

    Add database dependency module foundation

---
+++ app/src/main/java/com/wbeauty/crm/data/local/database/DatabaseModule.kt
@@ -0,0 +1,10 @@
+package com.wbeauty.crm.data.local.database
+
+/**
+ * Database dependency configuration placeholder.
+ *
+ * Will contain Room and dependency injection setup.
+ */
+object DatabaseModule {
+    const val VERSION = 1
+}
