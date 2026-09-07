8c37ec07d44597789619d3dca527aeaa19431192
waxew <4622.ar@gmail.com>
2026-09-07 13:19:13 +0330

    ایجاد اسکلت Android Gradle برای Build واقعی پروژه

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,6 +12,6 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 1
-        versionName = "1.0.0"
+        versionName = "0.1"
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,12 +6,9 @@ pluginManagement {
     }
 }
 
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-    }
+plugins {
+    id("com.android.application") version "8.5.2" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
 }
 
 rootProject.name = "W-Beauty_CRM"
