974c18df9c6e995dcbe621194104171af17a801c
waxew <4622.ar@gmail.com>
2026-09-07 12:18:20 +0330

    Add Room database foundation AppDatabase

---
+++ app/src/main/java/com/wbeauty/crm/data/local/database/AppDatabase.kt
@@ -0,0 +1,13 @@
+package com.wbeauty.crm.data.local.database
+
+/**
+ * Room database entry point.
+ *
+ * This file is the foundation for local offline storage.
+ * Entities and DAOs will be registered as modules are added.
+ */
+class AppDatabase {
+    companion object {
+        const val DATABASE_NAME = "w_beauty_crm.db"
+    }
+}
