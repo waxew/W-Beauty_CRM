f5c661c6060fbedcdfcacb5639e63f3b2055fecf
waxew <4622.ar@gmail.com>
2026-09-07 13:45:11 +0330

    حذف CustomerActions تکراری و انتقال رویدادها به مسیر اصلی ViewModel

--- app/src/main/java/com/wbeauty/crm/feature/customer/CustomerActions.kt
+++
@@ -1,7 +0,0 @@
-package com.wbeauty.crm.feature.customer
-
-sealed class CustomerAction {
-    data class NameChanged(val value: String) : CustomerAction()
-    data class PhoneChanged(val value: String) : CustomerAction()
-    data object Save : CustomerAction()
-}
