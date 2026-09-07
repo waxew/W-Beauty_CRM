0cdea5dd1b4907706b125a055848d711d8cdc2a5
waxew <4622.ar@gmail.com>
2026-09-07 14:36:48 +0330

    Add business theme configuration layer

---
+++ app/src/main/java/com/wbeauty/crm/business/config/BusinessThemeConfig.kt
@@ -0,0 +1,8 @@
+package com.wbeauty.crm.business.config
+
+import androidx.compose.ui.graphics.Color
+
+object BusinessThemeConfig {
+    val primaryColor: Color = Color(0xFF000000)
+    val secondaryColor: Color = Color(0xFFFFFFFF)
+}
