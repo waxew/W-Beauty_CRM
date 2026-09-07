4205d5126141006f4dc770798da895014b827eeb
waxew <4622.ar@gmail.com>
2026-09-07 14:36:56 +0330

    Add business text configuration layer

---
+++ app/src/main/java/com/wbeauty/crm/business/config/BusinessTextConfig.kt
@@ -0,0 +1,7 @@
+package com.wbeauty.crm.business.config
+
+object BusinessTextConfig {
+    const val customerTitle = "Customers"
+    const val reservationTitle = "Reservations"
+    const val invoiceTitle = "Invoices"
+}
