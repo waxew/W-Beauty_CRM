b93991649a313c5d4dcd344f91fb9e8a190a634b
waxew <4622.ar@gmail.com>
2026-09-07 12:49:24 +0330

    اضافه کردن UseCase دریافت مشتریان

---
+++ app/src/main/java/com/wbeauty/crm/feature/customer/domain/usecase/GetCustomersUseCase.kt
@@ -0,0 +1,8 @@
+package com.wbeauty.crm.feature.customer.domain.usecase
+
+// دریافت لیست مشتریان از لایه دامنه
+class GetCustomersUseCase {
+    fun execute() {
+        // منطق دریافت مشتریان در این مرحله اضافه می‌شود
+    }
+}
