a69dacbdccd4eb70ec030e2a7e5dfd93a2f2107c
waxew <4622.ar@gmail.com>
2026-09-07 13:02:04 +0330

    آماده سازی ماژول اتصال مشتری به Room

---
+++ app/src/main/java/com/wbeauty/crm/data/local/database/CustomerRoomModule.kt
@@ -0,0 +1,10 @@
+package com.wbeauty.crm.data.local.database
+
+/**
+ * محل اتصال تنظیمات دیتابیس مشتری.
+ *
+ * در مرحله Hilt تکمیل می‌شود.
+ */
+object CustomerRoomModule {
+    const val TABLE_NAME = "customers"
+}
