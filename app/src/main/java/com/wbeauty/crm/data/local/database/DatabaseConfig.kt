1f2784e8b395c07f937af261ecd8e54594527d87
waxew <4622.ar@gmail.com>
2026-09-07 13:04:32 +0330

    تکمیل پایه تنظیمات Room و DI دیتابیس

---
+++ app/src/main/java/com/wbeauty/crm/data/local/database/DatabaseConfig.kt
@@ -0,0 +1,8 @@
+package com.wbeauty.crm.data.local.database
+
+/**
+ * تنظیمات اصلی دیتابیس Room برنامه
+ */
+object DatabaseConfig {
+    const val DATABASE_NAME = "wbeauty_crm.db"
+}
---
+++ app/src/main/java/com/wbeauty/crm/di/DatabaseModule.kt
@@ -0,0 +1,8 @@
+package com.wbeauty.crm.di
+
+/**
+ * ماژول وابستگی دیتابیس
+ * پایه آماده برای اتصال Hilt
+ */
+object DatabaseModule {
+}
