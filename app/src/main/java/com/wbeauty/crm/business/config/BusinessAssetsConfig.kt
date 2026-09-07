25b8aa0e613be898d98c9a990996a19cd63ae8e3
waxew <4622.ar@gmail.com>
2026-09-07 14:42:16 +0330

    Add business asset configuration layer

---
+++ app/src/main/java/com/wbeauty/crm/business/config/BusinessAssetsConfig.kt
@@ -0,0 +1,7 @@
+package com.wbeauty.crm.business.config
+
+object BusinessAssetsConfig {
+    val logo: String = "business_logo"
+    val appIcon: String = "business_icon"
+    val imagesPath: String = "business/assets"
+}
