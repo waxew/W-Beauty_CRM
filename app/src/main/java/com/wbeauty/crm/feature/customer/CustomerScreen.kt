a11032c13cb96a856831c187440c1a2389a6fd33
waxew <4622.ar@gmail.com>
2026-09-07 13:47:57 +0330

    اتصال CustomerScreen به ViewModel نهایی

--- app/src/main/java/com/wbeauty/crm/feature/customer/CustomerScreen.kt
+++ app/src/main/java/com/wbeauty/crm/feature/customer/CustomerScreen.kt
@@ -2,8 +2,11 @@ package com.wbeauty.crm.feature.customer
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
-fun CustomerScreen() {
+fun CustomerScreen(
+    viewModel: CustomerViewModel = hiltViewModel()
+) {
     Text(text = "لیست مشتریان")
 }
