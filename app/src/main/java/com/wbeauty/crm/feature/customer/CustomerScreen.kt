4d5b7acc90a703a8c574cc5b2a6fc5581600a872
waxew <4622.ar@gmail.com>
2026-09-07 13:34:55 +0330

    افزودن صفحه اولیه Compose مشتری

---
+++ app/src/main/java/com/wbeauty/crm/feature/customer/CustomerScreen.kt
@@ -0,0 +1,9 @@
+package com.wbeauty.crm.feature.customer
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CustomerScreen() {
+    Text(text = "لیست مشتریان")
+}
