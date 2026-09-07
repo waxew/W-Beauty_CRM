8f4a7f3515b9ac60230b24b19818c98f0fbf1e2b
waxew <4622.ar@gmail.com>
2026-09-07 13:40:20 +0330

    ثبت بررسی یکپارچگی مسیر مشتری قبل از Build

---
+++ app/src/main/java/com/wbeauty/crm/feature/customer/CustomerRepositoryIntegrationCheck.kt
@@ -0,0 +1,9 @@
+package com.wbeauty.crm.feature.customer
+
+/**
+ * بررسی یکپارچگی مسیر مشتری.
+ * این کلاس نقطه ثبت مرحله اتصال UI به لایه داده است.
+ */
+object CustomerRepositoryIntegrationCheck {
+    const val STATUS = "UI -> ViewModel -> UseCase -> Repository -> Room"
+}
