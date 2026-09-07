b6088fd682d6dc689c8f60ba72938e79547b78e0
waxew <4622.ar@gmail.com>
2026-09-07 18:43:01 +0330

    Add mobile store business profile instance

---
+++ app/src/main/java/com/wcrm/engine/businessprofile/profiles/mobile_store/ProfileConfig.kt
@@ -0,0 +1,10 @@
+package com.wcrm.engine.businessprofile.profiles.mobile_store
+
+import com.wcrm.engine.businessprofile.BusinessProfile
+
+object MobileStoreProfile {
+    val profile = BusinessProfile(
+        id = "mobile_store",
+        name = "Mobile Store"
+    )
+}
