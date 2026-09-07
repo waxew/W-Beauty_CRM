9874931c4d45561541ca25742890d0bc1cb1b03d
waxew <4622.ar@gmail.com>
2026-09-07 17:57:56 +0330

    refactor: migrate application namespace from beauty to crm engine

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,11 +6,11 @@ plugins {
 }
 
 android {
-    namespace = "com.wbeauty.crm"
+    namespace = "com.wcrm.engine"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.wbeauty.crm"
+        applicationId = "com.wcrm.engine"
         minSdk = 26
         targetSdk = 35
         versionCode = 1
