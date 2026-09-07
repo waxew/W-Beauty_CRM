7e67a7f4a719fb7bcbb5a221ce5d2e67dbb0a455
waxew <4622.ar@gmail.com>
2026-09-07 13:35:41 +0330

    افزودن اسکلت صفحه افزودن مشتری Compose

---
+++ app/src/main/java/com/wbeauty/crm/feature/customer/CustomerAddScreen.kt
@@ -0,0 +1,8 @@
+package com.wbeauty.crm.feature.customer
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CustomerAddScreen() {
+    // فرم افزودن مشتری در مرحله بعد تکمیل می‌شود
+}
