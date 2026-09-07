2b24f26c2a430de12ac641ef96a6c39a1413dd2e
waxew <4622.ar@gmail.com>
2026-09-07 14:34:53 +0330

    Add default beauty salon business profile

---
+++ app/src/main/java/com/wbeauty/crm/business/config/BusinessProfile.kt
@@ -0,0 +1,13 @@
+package com.wbeauty.crm.business.config
+
+object BusinessProfile {
+    val current = BusinessConfig(
+        id = "beauty_salon_01",
+        appName = "Beauty CRM",
+        businessName = "Beauty Salon 01",
+        packageName = "com.wbeauty.crm",
+        primaryColor = 0xFFE91E63,
+        customerTitle = "Customers",
+        reservationTitle = "Reservations"
+    )
+}
