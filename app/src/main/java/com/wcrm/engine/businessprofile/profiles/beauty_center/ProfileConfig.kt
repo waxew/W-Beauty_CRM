3393e59cd60c0cd26b26bcab3a49427e9bce7c09
waxew <4622.ar@gmail.com>
2026-09-07 18:43:08 +0330

    Add beauty center business profile instance

---
+++ app/src/main/java/com/wcrm/engine/businessprofile/profiles/beauty_center/ProfileConfig.kt
@@ -0,0 +1,10 @@
+package com.wcrm.engine.businessprofile.profiles.beauty_center
+
+import com.wcrm.engine.businessprofile.BusinessProfile
+
+object BeautyCenterProfile {
+    val profile = BusinessProfile(
+        id = "beauty_center",
+        name = "Beauty Center"
+    )
+}
