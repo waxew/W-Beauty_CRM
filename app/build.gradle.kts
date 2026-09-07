caafc3597cfe38ea170a375799f0f0036f6ea108
waxew <4622.ar@gmail.com>
2026-09-07 13:58:49 +0330

    ایجاد تنظیمات Gradle ماژول app

---
+++ app/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("com.google.dagger.hilt.android")
+}
+
+android {
+    namespace = "com.wbeauty.crm"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.wbeauty.crm"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.activity:activity-compose:1.9.3")
+    implementation("androidx.compose.material3:material3:1.3.1")
+}
