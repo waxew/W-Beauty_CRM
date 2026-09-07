1080d915eb4750167347abc1c184b9f1d099e8e7
waxew <4622.ar@gmail.com>
2026-09-07 13:55:32 +0330

    Complete Customer DAO CRUD support

--- app/src/main/java/com/wbeauty/crm/data/local/dao/CustomerDao.kt
+++ app/src/main/java/com/wbeauty/crm/data/local/dao/CustomerDao.kt
@@ -1,12 +1,18 @@
 package com.wbeauty.crm.data.local.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.wbeauty.crm.data.local.entity.CustomerEntity
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface CustomerDao {
-    @Query("SELECT * FROM customers")
+
+    @Query("SELECT * FROM customers ORDER BY id DESC")
     fun getAll(): Flow<List<CustomerEntity>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(customer: CustomerEntity)
 }
