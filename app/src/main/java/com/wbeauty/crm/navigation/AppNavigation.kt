f2df5e9feec9b17e14e3e925eba9d5ccdcfb3e19
waxew <4622.ar@gmail.com>
2026-09-07 13:35:00 +0330

    افزودن Navigation اولیه Compose

---
+++ app/src/main/java/com/wbeauty/crm/navigation/AppNavigation.kt
@@ -0,0 +1,9 @@
+package com.wbeauty.crm.navigation
+
+import androidx.compose.runtime.Composable
+import com.wbeauty.crm.feature.customer.CustomerScreen
+
+@Composable
+fun AppNavigation() {
+    CustomerScreen()
+}
