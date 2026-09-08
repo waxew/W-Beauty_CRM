0d5383b75e7e2e98c6928fe9087c17d7aae40520
waxew <4622.ar@gmail.com>
2026-09-08 19:11:15 +0330

    Connect app module to runtime module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,6 +29,7 @@ android {
 dependencies {
     implementation(project(":core"))
     implementation(project(":business_profile"))
+    implementation(project(":runtime"))
 
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.activity:activity-compose:1.9.3")
