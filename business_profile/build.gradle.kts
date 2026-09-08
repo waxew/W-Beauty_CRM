e0396a743eead01308b4ab43e526a2d42294cee2
waxew <4622.ar@gmail.com>
2026-09-08 17:47:00 +0330

    Fix business_profile module for Android library compatibility

--- business_profile/build.gradle.kts
+++ business_profile/build.gradle.kts
@@ -1,10 +1,17 @@
 plugins {
-    id("org.jetbrains.kotlin.jvm")
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
 }
 
-kotlin {
-    jvmToolchain(17)
-}
+android {
+    namespace = "com.wcrm.engine.businessprofile"
+    compileSdk = 35
 
-dependencies {
+    defaultConfig {
+        minSdk = 26
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
