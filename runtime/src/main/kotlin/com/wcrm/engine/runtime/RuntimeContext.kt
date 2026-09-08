9aa4240f44aa37915346e84c690998b787017aa3
waxew <4622.ar@gmail.com>
2026-09-08 18:40:52 +0330

    Add RuntimeContext

---
+++ runtime/src/main/kotlin/com/wcrm/engine/runtime/RuntimeContext.kt
@@ -0,0 +1,9 @@
+package com.wcrm.engine.runtime
+
+/**
+ * Runtime state container shared by application services.
+ */
+data class RuntimeContext(
+    val activeProfile: String,
+    val enabledFeatures: Set<String> = emptySet()
+)
