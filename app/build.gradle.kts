6fb804de5830be65e684e30be9959d7179107a2f
waxew <4622.ar@gmail.com>
2026-09-08 17:47:29 +0330

    Connect app module with core and business profile modules

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,24 +27,22 @@ android {
 }
 
 dependencies {
+    implementation(project(":core"))
+    implementation(project(":business_profile"))
+
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.activity:activity-compose:1.9.3")
-
     implementation(platform("androidx.compose:compose-bom:2024.12.01"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     debugImplementation("androidx.compose.ui:ui-tooling")
-
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.7")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
-
     implementation("androidx.navigation:navigation-compose:2.8.5")
-
     implementation("com.google.dagger:hilt-android:2.52")
     ksp("com.google.dagger:hilt-compiler:2.52")
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
-
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
     ksp("androidx.room:room-compiler:2.6.1")
