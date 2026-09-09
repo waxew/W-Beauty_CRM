3fe4d4cfa1e0b7cab8b38a6791646f1d265e084e
waxew <4622.ar@gmail.com>
2026-09-09 18:41:18 +0330

    restore missing Business Profile ModuleType model

---
+++ app/src/main/java/com/wbeauty/crm/business_profile/ModuleType.kt
@@ -0,0 +1,16 @@
+package com.wbeauty.crm.business_profile
+
+/**
+ * ماژول های اصلی CRM.
+ */
+enum class ModuleType {
+    CUSTOMER,
+    PRODUCT,
+    INVENTORY,
+    SALES,
+    INVOICE,
+    ACCOUNTING,
+    WARRANTY,
+    REPAIR,
+    DELIVERY
+}
